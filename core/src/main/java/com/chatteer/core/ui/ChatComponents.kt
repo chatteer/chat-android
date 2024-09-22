8c2656026860e3bbe7e8a668c73a98d15ba9eaa9
sieunju <j.sieun73@gmail.com>
2024-09-22 11:11:38 +0900

    feat: core 모듈 기능 구현 (ui component) - 공통 헤더 ui 구현 - 필요한 리소스 추가

--- core/src/main/java/com/chatteer/core/ui/ChatComponents.kt
+++ core/src/main/java/com/chatteer/core/ui/ChatComponents.kt
@@ -2,17 +2,24 @@ package com.chatteer.core.ui
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -21,12 +28,21 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.boundsInRoot
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -38,6 +54,7 @@ import com.bumptech.glide.integration.compose.GlideImage
 import com.bumptech.glide.integration.compose.placeholder
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.chatteer.core.R
+import kotlin.math.abs
 
 /**
  * Description : Chat UI Component
@@ -165,5 +182,164 @@ object ChatComponents {
         ) { requestBuilder ->
             requestBuilder.diskCacheStrategy(DiskCacheStrategy.NONE)
         }
+    }
+
+    @Composable
+    fun HeaderAndContents(
+        navigationHeader: @Composable BoxScope.() -> Unit,
+        collapseHeader: (@Composable BoxScope.() -> Unit)? = null,
+        stickyHeader: (@Composable BoxScope.() -> Unit)? = null,
+        contents: @Composable BoxScope.() -> Unit
+    ) {
+        val density = LocalDensity.current
+        val scrollState = rememberScrollState()
+        val navigationHeight = 56.dp
+        val navigationHeightPx = with(density) { 56.dp.toPx() }
+        var collapseHeightPx by remember { mutableStateOf(0F) }
+        var stickyHeightPx by remember { mutableStateOf(0F) }
+        var headerOffsetPx by remember { mutableStateOf(0F) }
+        var collapseVisiblePercentage by remember { mutableStateOf(0F) }
+
+        val nestedScrollConnection = remember {
+            object : NestedScrollConnection {
+                override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
+                    val dy = available.y
+                    // 접히는 레이아웃 숨겨진 상태
+                    return if (dy < 0) {
+                        // 터치가 아래에서 위로 (아래로 스크롤)
+                        if (collapseHeader != null && collapseVisiblePercentage <= 0f) {
+                            println("Timber onPreScroll 접히는 레이아웃이 접혔을때 $headerOffsetPx")
+                            Offset.Zero
+                        } else {
+                            println("Timber 아래로 스크롤합니다. 보편적으로 ${headerOffsetPx}, $dy")
+                            scrollState.dispatchRawDelta(dy * -1)
+                            val offset = headerOffsetPx.plus(dy)
+                            headerOffsetPx = offset.coerceIn(-collapseHeightPx, 0F)
+                            Offset(0f, dy)
+                        }
+                    } else {
+                        // 터치가 위에서 아래로 (위로 스크롤)
+                        // 접히는 뷰가 접혔을때
+                        if (collapseHeader != null && collapseVisiblePercentage <= 0F) {
+                            // scrollState.dispatchRawDelta(dy)
+                            // val offset = headerOffsetPx.plus(dy)
+                            // headerOffsetPx = offset.coerceIn(-collapseHeightPx, 0F)
+                            val consumed =  scrollState.dispatchRawDelta(-dy)
+                            println("Timber 위로 스크롤, 접혔을때 ${consumed}")
+                            Offset.Zero
+                        } else {
+                            println("Timber 여기탑니다. $headerOffsetPx")
+                            val offset = headerOffsetPx.plus(dy)
+                            headerOffsetPx = offset.coerceIn(-collapseHeightPx, 0F)
+                            Offset.Zero
+                        }
+                    }
+//                    return if (collapseHeader != null && collapseVisiblePercentage <= 0f) {
+//                        println("Timber onPreScroll 접히는 레이아웃이 접혔을때 $headerOffsetPx")
+//                        Offset.Zero
+//                    } else if (dy < 0) {
+//                        println("Timber onPreScroll else if dy<0 $headerOffsetPx")
+//                        // 터치가 아래에서 위로
+//                        scrollState.dispatchRawDelta(dy * -1)
+//                        val offset = headerOffsetPx.plus(dy)
+//                        // -headerMaxHeightPx ~ 0F
+//                        headerOffsetPx = offset.coerceIn(-collapseHeightPx, 0F)
+//                        Offset(0f,dy)
+//                    } else {
+//                        val offset = headerOffsetPx.plus(dy)
+//                        // -headerMaxHeightPx ~ 0F
+//                        headerOffsetPx = offset.coerceIn(-collapseHeightPx, 0F)
+//                        println("Timber onPreScroll else $headerOffsetPx")
+//                        Offset.Zero
+//                    }
+//                    val offset = headerOffsetPx.plus(dy)
+//                    // -headerMaxHeightPx ~ 0F
+//                    headerOffsetPx = offset.coerceIn(-collapseHeightPx, 0F)
+//                    println("Timber $headerOffsetPx")
+//                    return Offset.Zero
+                }
+            }
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .nestedScroll(nestedScrollConnection)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(navigationHeight)
+                    .background(ChatTheme.color.primary)
+            ) { navigationHeader() }
+            if (collapseHeader != null) {
+//                Box(
+//                    modifier = Modifier
+//                        .padding(top = navigationHeight)
+//                        .fillMaxWidth()
+//                        .onSizeChanged { collapseHeightPx = it.height.toFloat() }
+//                        .verticalScroll(scrollState)
+//                        .onGloballyPositioned { coordinates ->
+//                            val height = coordinates.boundsInRoot().height
+//                            collapseVisiblePercentage = height / collapseHeightPx
+//                        }
+//                        .offset(y = with(density) {
+//                            headerOffsetPx.toDp()
+//                        })
+//                        .alpha(collapseVisiblePercentage)
+//                ) { collapseHeader() }
+                Column(
+                    modifier = Modifier
+                        .padding(top = navigationHeight)
+                        .fillMaxWidth()
+                        .onSizeChanged { collapseHeightPx = it.height.toFloat() }
+                        .verticalScroll(scrollState)
+                        .offset(y = with(density) {
+                            headerOffsetPx.toDp()
+                        })
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentHeight()
+                            .onGloballyPositioned { coordinates ->
+                                val height = coordinates.boundsInRoot().height
+                                collapseVisiblePercentage = height / collapseHeightPx
+                            }
+                            .alpha(collapseVisiblePercentage)
+                    ) { collapseHeader() }
+                }
+            }
+            if (stickyHeader != null) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = with(density) {
+                            headerOffsetPx
+                                .plus(navigationHeightPx)
+                                .plus(collapseHeightPx)
+                                .toDp()
+                        })
+                        .onSizeChanged { stickyHeightPx = it.height.toFloat() }
+                ) { stickyHeader() }
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = with(density) {
+                        abs(
+                            headerOffsetPx
+                                .plus(navigationHeightPx)
+                                .plus(collapseHeightPx)
+                                .plus(stickyHeightPx)
+                        ).toDp()
+                    })
+            ) { contents() }
+        }
+
+//        LaunchedEffect(collapseHeight) {
+//            collapseHeightPx = with(density) {
+//                this.density.toPx
+//            }
+//        }
     }
 }
--- core/src/main/java/com/chatteer/core/ui/ChatThemeExample.kt
+++ core/src/main/java/com/chatteer/core/ui/ChatThemeExample.kt
@@ -1,39 +1,50 @@
 package com.chatteer.core.ui
 
+import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import androidx.compose.ui.input.nestedscroll.NestedScrollSource
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.boundsInRoot
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.max
 import com.chatteer.core.R
-import timber.log.Timber
-import kotlin.math.roundToInt
 
 /**
  * Description : 채티어 테마 예시
@@ -42,33 +53,29 @@ import kotlin.math.roundToInt
  */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(showBackground = true, showSystemUi = true, uiMode = Configuration.UI_MODE_NIGHT_NO)
 private fun HeaderType1() {
-    val toolbarHeight = 200.dp
-    val toolbarMinHeight = 56.dp
-    val toolbarHeightPx = with(LocalDensity.current) { toolbarHeight.roundToPx().toFloat() }
-    var toolbarOffsetHeightPx by remember { mutableFloatStateOf(0f) }
-    val toolbarOffsetHeightDp = with(LocalDensity.current) { toolbarOffsetHeightPx.toDp() }
+    var isHeaderHide = false
+    val scrollState = rememberScrollState()
     val nestedScrollConnection = remember {
         object : NestedScrollConnection {
 
             override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
-                toolbarOffsetHeightPx += available.y
-                return Offset.Zero
-            }
-
-            override fun onPostScroll(
-                consumed: Offset,
-                available: Offset,
-                source: NestedScrollSource
-            ): Offset {
-                toolbarOffsetHeightPx -= available.y
-                return Offset.Zero
+                val dy = available.y
+                println("Scroll $dy")
+                return if (isHeaderHide) {
+                    Offset.Zero
+                } else if (dy < 0) {
+                    // 터치가 아래에서 위로, 화면을 위로 올릴때 부모가 먼저 스크롤 받기
+                    scrollState.dispatchRawDelta(dy * -1)
+                    Offset(0f, dy)
+                } else {
+                    // 위에서 아래로 내릴때 부모가 스크롤 이벤트 받지 않도록
+                    Offset.Zero
+                }
             }
         }
     }
-
-    println("Scroll $toolbarOffsetHeightPx, $toolbarHeightPx")
 
     Scaffold(
         modifier = Modifier
@@ -78,31 +85,179 @@ private fun HeaderType1() {
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
+                    .height(56.dp)
                     .background(ChatTheme.color.primary)
-                    .height(Math.max(-toolbarHeightPx,toolbarOffsetHeightPx).minus(toolbarHeightPx).dp)
-                    // .height(max(toolbarHeight + toolbarOffsetHeightDp, 56.dp))
-                 // .offset(y = toolbarOffsetHeightPx.dp)
             ) {
                 Text(
-                    text = "헤더 타입 1 ${toolbarOffsetHeightDp}",
+                    text = "친구",
+                    modifier = Modifier
+                        .align(Alignment.CenterStart)
+                        .padding(start = 10.dp),
                     color = ChatTheme.color.white,
                     style = ChatTheme.text.h3M
+                )
+                Image(
+                    painter = painterResource(R.drawable.ic_add_chat),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .align(Alignment.CenterEnd)
+                        .padding(end = 10.dp)
                 )
             }
         }
     ) { innerPadding ->
-        LazyColumn(
-            contentPadding = innerPadding,
+        Box(
             modifier = Modifier
+                .padding(innerPadding)
                 .fillMaxSize()
         ) {
-            items(50) { index ->
-                Text(
-                    text = "Item $index",
+            var globalHeight by remember { mutableStateOf(0) }
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Transparent)
+                    .onSizeChanged { size ->
+                        globalHeight = size.height
+                    }
+                    .nestedScroll(nestedScrollConnection)
+                    .verticalScroll(scrollState)
+            ) {
+                // Header
+                HeaderContents(
+                    header = {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(ChatTheme.color.primary)
+                                .height(200.dp)
+                        ) {
+                            Text(
+                                text = "여기가 헤더입니까?",
+                                modifier = Modifier.padding(20.dp)
+                            )
+                        }
+                    },
+                    onHide = { isHide ->
+                        isHeaderHide = isHide
+                    }
+                )
+                LazyColumn(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(16.dp)
+                        .height(globalHeight.dp)
+                ) {
+                    items(50) { index ->
+                        Text(
+                            text = "Item $index",
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun HeaderContents(
+    header: @Composable ColumnScope.() -> Unit,
+    onHide: (Boolean) -> Unit
+) {
+    var contentHeight by remember { mutableStateOf(0) }
+    var visiblePercentage by remember { mutableStateOf(1f) }
+
+    LaunchedEffect(visiblePercentage) {
+        if (visiblePercentage <= 0f) {
+            onHide(true)
+        } else {
+            onHide(false)
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .onGloballyPositioned { layoutCoordinates ->
+                visiblePercentage = layoutCoordinates.boundsInRoot().height / contentHeight
+            }
+            .onSizeChanged { contentHeight = it.height }
+            .alpha(visiblePercentage)
+    ) { this.header() }
+}
+
+@Composable
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    uiMode = Configuration.UI_MODE_NIGHT_NO
+)
+private fun HeaderExample1() {
+    val lazyScrollState = rememberLazyListState()
+    Scaffold {
+        ChatComponents.HeaderAndContents(
+            navigationHeader = {
+                Text(
+                    text = "친구",
+                    style = ChatTheme.text.h,
+                    color = ChatTheme.color.white,
+                    modifier = Modifier
+                        .align(Alignment.CenterStart)
+                        .padding(start = 15.dp)
                 )
+                Image(
+                    painter = painterResource(R.drawable.ic_add_chat),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .align(Alignment.CenterEnd)
+                        .padding(end = 15.dp)
+                )
+            },
+            collapseHeader = {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp)
+                        .background(ChatTheme.color.primary)
+                ) {
+                    ChatComponents.ImageLoader(
+                        imageUrl = "https://til.qtzz.synology.me/resources/img/20210921/1632238064795dwalkkz7dea.png",
+                        modifier = Modifier
+                            .size(100.dp)
+                            .align(Alignment.Center)
+                            .clip(CircleShape)
+                            .border(2.dp, ChatTheme.color.white, CircleShape)
+                    )
+                }
+            },
+            stickyHeader = {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(50.dp)
+                        .background(ChatTheme.color.gray3),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "StickyHeader...",
+                        style = ChatTheme.text.h
+                    )
+                }
+            }
+        ) {
+            LazyColumn(
+                state = lazyScrollState
+            ) {
+                items(100) { index ->
+                    Text(
+                        text = "Item $index",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                    )
+                }
             }
         }
     }
