8b72e8cb9f7c9bffc5610f40bc9e5bcf5e7a888b
sieunju <j.sieun73@gmail.com>
2024-09-28 17:51:19 +0900

    chore: 누락된 paddingvalue 추가

--- features/main/src/main/java/com/chatteer/main/component/MainNavHost.kt
+++ features/main/src/main/java/com/chatteer/main/component/MainNavHost.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -23,6 +24,7 @@ internal fun MainNavHost(
 ) {
     Box(
         modifier = Modifier
+            .padding(paddingValues)
             .fillMaxSize()
             .background(ChatTheme.color.white)
     ) {
